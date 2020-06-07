0c0475ae313419abe56a397310e1b56ecc703947
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-06-07 19:17:59 +0200

    Fix login and add TODOs

---
+++ app/src/main/java/com/example/pa4al/LoginDTO.java
@@ -0,0 +1,11 @@
+package com.example.pa4al;
+
+public class LoginDTO {
+    final String username;
+    final String password;
+
+    public LoginDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
